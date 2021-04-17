e114e25ffa79a849f3b27936beef49d65651c362
charliew2620 <cywang3@illinois.edu>
2021-04-17 15:22:31 -0500

    Creates the bird class with methods.

--- include/bird.h
+++ include/bird.h
@@ -6,7 +6,34 @@
 
 namespace flappybird {
 
+using glm::vec2;
+
 class Bird {
+
+ public:
+  Bird(const vec2 &position, const vec2 &velocity, const vec2& acceleration, const std::string &color, const double radius,
+       const double mass);
   
+  void Draw();
+  
+  void UpdateBird();
+  
+  // getters
+  const vec2& GetPosition() const;
+  const vec2& GetVelocity() const;
+  const vec2& GetAcceleration() const;
+  const std::string& GetColor() const;
+  const double& GetRadius() const;
+  const double& GetMass() const;
+  
+
+ private:
+  vec2 position_;
+  vec2 velocity_;
+  vec2 acceleration_;
+  std::string color_;
+  double radius_;
+  double mass_;
+
 };
 }
--- src/bird.cc
+++ src/bird.cc
@@ -1,1 +1,50 @@
 #include <bird.h>
+
+namespace flappybird {
+
+Bird::Bird(const glm::vec2 &position,
+           const glm::vec2 &velocity,
+           const vec2 &acceleration,
+           const std::string &color,
+           const double radius,
+           const double mass) {
+  position_ = position;
+  velocity_ = velocity;
+  acceleration_ = acceleration;
+  color_ = color;
+  radius_ = radius;
+  mass_ = mass;
+
+}
+
+void Bird::Draw() {
+  ci::gl::color(ci::Color(color_.c_str()));
+  ci::gl::drawSolidCircle(vec2(position_), (float) radius_);
+}
+
+void Bird::UpdateBird() {
+  position_++;
+}
+
+const vec2 &Bird::GetPosition() const {
+  return position_;
+}
+
+const vec2 &Bird::GetVelocity() const {
+  return velocity_;
+}
+const vec2 &Bird::GetAcceleration() const {
+  return acceleration_;
+}
+const std::string &Bird::GetColor() const {
+  return color_;
+}
+
+const double &Bird::GetRadius() const {
+  return radius_;
+}
+
+const double &Bird::GetMass() const {
+  return mass_;
+}
+}
